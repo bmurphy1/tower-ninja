745d8b7c428fdcf67f6d6a13b7687f3fd2dbca40
Brian Murphy <bmurphy1@mac.com>
2015-01-29 18:37:45 -0500

    Large Object Oriented refactor

--- public/tower_ninja.js
+++ public/tower_ninja.js
@@ -1,128 +1,129 @@
-var game = new Phaser.Game(375, 667, Phaser.AUTO, 'gameContainer', {preload: preload, create: create, update: update});
+var TowerNinja = {};
 
-
-// ======================= PRELOAD ================================= //
-function preload() {
-    game.load.image('sky', 'images/sky.png');
-    game.load.image('ground', 'images/platform.png');
-    game.load.image('wall', 'images/wall.png');
-    game.load.image('star', 'images/star.png');
-    game.load.spritesheet('dude', 'images/dude.png', 32, 48);
+TowerNinja.Game = function(game) {
+    this.platforms;
+    this.walls;
+    this.player;
+    this.cursors;
+    this.jumpButton;
 };
 
-var platforms;
-var walls;
-var player;
-var cursors;
-var jumpButton;
+TowerNinja.Game.prototype = {
+
+    // ======================= PRELOAD ================================= //
+    preload: function() {
+        this.load.image('sky', 'images/sky.png');
+        this.load.image('ground', 'images/platform.png');
+        this.load.image('wall', 'images/wall.png');
+        this.load.image('star', 'images/star.png');
+        this.load.spritesheet('dude', 'images/dude.png', 32, 48);
+    },
+
+    // ======================== CREATE ================================= //
+    create: function() {
+
+        this.physics.startSystem(Phaser.Physics.ARCADE);
+        cursors = this.input.keyboard.createCursorKeys();
+        jumpButton = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
 
-// ======================== CREATE ================================= //
-function create() {
+        // this.physics.arcade.gravity.x = 300;
 
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-    cursors = game.input.keyboard.createCursorKeys();
-    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        sky = this.add.sprite(0,0,'sky');
+        sky.scale.setTo(1,2);
 
-//    this.physics.arcade.gravity.x = 300;
+        // PLATFORMS
+        platforms = this.add.group();
+        platforms.enableBody = true;
+        var ground = platforms.create(0, this.world.height - 64, 'ground');
+        var ceiling = platforms.create(0, 0, 'ground');
 
-    sky = game.add.sprite(0,0,'sky');
-    sky.scale.setTo(1,2);
+        ground.scale.setTo(2,2);
+        ceiling.scale.setTo(2,2);
 
-    // PLATFORMS
-    platforms = game.add.group();
-    platforms.enableBody = true;
-    var ground = platforms.create(0, game.world.height - 64, 'ground');
-    var ceiling = platforms.create(0, 0, 'ground');
+        ground.body.immovable = true;
+        ceiling.body.immovable = true;
 
-    ground.scale.setTo(2,2);
-    ceiling.scale.setTo(2,2);
+        // LEDGES
+    //    var ledge = platforms.create(200, 400, 'ground');
+    //    this.physics.arcade.enable(ledge);
+    //    ledge.body.immovable = true;
+    //    ledge.body.velocity.x = -25;
+    //    var ledge = platforms.create(-150, 200, 'ground');
+    //    ledge.body.immovable = true;
 
-    ground.body.immovable = true;
-    ceiling.body.immovable = true;
+        // WALLS
+        walls = this.add.group();
+        walls.enableBody = true;
+        var leftWall = walls.create(0, 0, 'wall');
+        var rightWall = walls.create(this.world.width-32, 0, 'wall');
+        leftWall.body.immovable = true;
+        rightWall.body.immovable = true;
+        leftWall.scale.setTo(1,2);
+        rightWall.scale.setTo(1,2);
 
-    // LEDGES
-//    var ledge = platforms.create(200, 400, 'ground');
-//    game.physics.arcade.enable(ledge);
-//    ledge.body.immovable = true;
-//    ledge.body.velocity.x = -25;
-//    var ledge = platforms.create(-150, 200, 'ground');
-//    ledge.body.immovable = true;
+        // Ready Player One
+        this.player = this.add.sprite(48, this.world.height - 150, 'dude');
 
-    // WALLS
-    walls = game.add.group();
-    walls.enableBody = true;
-    var leftWall = walls.create(0, 0, 'wall');
-    var rightWall = walls.create(game.world.width-32, 0, 'wall');
-    leftWall.body.immovable = true;
-    rightWall.body.immovable = true;
-    leftWall.scale.setTo(1,2);
-    rightWall.scale.setTo(1,2);
+        this.physics.arcade.enable(this.player);
 
-    // Ready Player One
-    player = game.add.sprite(48, game.world.height - 150, 'dude');
+        this.player.anchor.setTo(.5, .5);
+        this.player.body.gravity.x = 40000;
+        this.player.body.collideWorldBounds = true;
+        this.player.body.velocity.x = 50;
 
-    game.physics.arcade.enable(player);
+        this.player.animations.add('left', [0,1,2,3], 10, true);
+        this.player.animations.add('right', [5,6,7,8], 10, true);
 
-    player.anchor.setTo(.5, .5);
-    player.body.gravity.x = 40000;
-    player.body.collideWorldBounds = true;
-    player.body.velocity.x = 50;
-
-    player.animations.add('left', [0,1,2,3], 10, true);
-    player.animations.add('right', [5,6,7,8], 10, true);
-
-    game.camera.follow(player);
-
-};
+        this.camera.follow(this.player);
+    },
 
 
-// ==================== UPDATE ====================================//
-function update() {
+    // ==================== UPDATE ====================================//
+    update: function() {
 
-    game.physics.arcade.collide(player, platforms);
-    game.physics.arcade.collide(player, walls);
+        this.physics.arcade.collide(this.player, platforms);
+        this.physics.arcade.collide(this.player, walls);
 
-    player.body.velocity.x = 0;
+        this.player.body.velocity.x = 0;
 
-    if (cursors.left.isDown) {
-        player.body.velocity.x = -150;
-//        player.animations.play('left');
-    } else if (cursors.right.isDown) {
-        player.body.velocity.x = 150;
-//        player.animations.play('right');
-    } else {
-//        player.animations.stop();
-        player.frame = 4;
-    }
+        // jumpButton.onDown.add(jump(player), this);
 
-    // Jumping back and forth
-    if (jumpButton.isDown && (player.body.touching.right || player.body.touching.left)) {
-        jump(player);
+    //     if (cursors.left.isDown) {
+    //         player.body.velocity.x = -150;
+    // //        player.animations.play('left');
+    //     } else if (cursors.right.isDown) {
+    //         player.body.velocity.x = 150;
+    // //        player.animations.play('right');
+    //     } else {
+    // //        player.animations.stop();
+    //         player.frame = 4;
+    //     }
 
+        // Jumping back and forth
+    //     if (jumpButton.onDown {
+    //         jump(player);
+
+    //     }
+    },
+
+
+    // ========================= OTHER ================================ //
+    jump: function() {
+        console.log('jump pressed');
+        if (this.player.body.touching.right || this.player.body.touching.left) {
+            if (this.player.body.gravity.x > 0) {
+                this.player.scale.x = -1;
+                this.player.body.velocity.x = -this.player.body.velocity.x;
+                this.player.body.gravity.x = -this.player.body.gravity.x;
+            } else {
+                this.player.scale.x = 1;
+                this.player.body.velocity.x = -this.player.body.velocity.x;
+                this.player.body.gravity.x = -this.player.body.gravity.x;
+            }
+        }
     }
 }
-
-
-// ========================= OTHER ================================ //
-function jump(player) {
-    console.log('jump pressed');
-    if (player.body.gravity.x > 0) {
-        player.scale.x = -1;
-        player.body.velocity.x = -player.body.velocity.x;
-        player.body.gravity.x = -player.body.gravity.x;
-    } else {
-        player.scale.x = 1;
-        player.body.velocity.x = -player.body.velocity.x;
-        player.body.gravity.x = -player.body.gravity.x;
-    }
-}
-
-
-
-
-
-
 
 
 
